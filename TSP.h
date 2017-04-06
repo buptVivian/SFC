7f8b140d5fcd9de5874139c085272c03d2627bca
buptVivian <vivian__915@126.com>
2017-04-06 17:27:37 +0800

    TSP.h

---
+++ TSP.h
@@ -0,0 +1,36 @@
+#include<algorithm>
+#include<cstdio>
+#include<queue>
+using namespace std;
+
+static const int MAX_N = 22;
+const int INF = 100000;
+static int n;//节点个数
+
+vector <vector<int>> cost;
+
+typedef struct Node
+{
+	bool visited[MAX_N];//标记哪些点走了
+	int s;//第一个点
+	int s_p;//第一个点的邻接点
+	int e;//最后一个点
+	int e_p;//最后一个点的邻接点
+	int k;//走过的点数
+	int sumv;//经过路径的距离
+	int lb;//目标函数的值（目标结果）
+	bool operator <(const Node &p)const
+	{
+		return p.lb < lb;//目标函数值小的先出队列
+	}
+};
+priority_queue<Node> pq;//创建一个优先队列
+int low, up;//下界和上界
+bool dfs_visited[MAX_N];//在dfs过程中搜索过
+
+int dfs(int u, int k, int l);
+void get_up();
+void get_low();
+int get_lb(Node p);
+int solve();
+int calculateOrder(vector<vector<int>> cost);
